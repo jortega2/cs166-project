a9d796ed63990fcf298e99178b16db9b0ba2c236
jortega2 <juvenal.a.ortega@gmail.com>
2022-03-07 15:38:49 -0800

    fixed more syntax erros

--- sql/src/create_tables.sql
+++ sql/src/create_tables.sql
@@ -41,7 +41,7 @@ CREATE TABLE MESSAGE(
 	sendTime timestamp,
 	deleteStatus integer,
 	status char(30) NOT NULL,
-	PRIMARY KEY(msgId);
+	PRIMARY KEY(msgId)
 	FOREIGN KEY (senderId) REFERENCES USR(userId),
 	FOREIGN KEY (receiverId) REFERENCES USR(userId));
 
@@ -50,5 +50,5 @@ CREATE TABLE CONNECTION_USR(
 	connectionId char(10) NOT NULL, 
 	status char(30) NOT NULL,
 	PRIMARY KEY(userId,connectionId),
-	FOREIGN KEY (userId) REFERENCES USR;
+	FOREIGN KEY (userId) REFERENCES USR
 	);
