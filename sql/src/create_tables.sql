3f9deea0b766522658eed00b2b8bb22e080648b5
jortega2 <juvenal.a.ortega@gmail.com>
2022-03-07 18:03:11 -0800

    added startup files

--- sql/src/create_tables.sql
+++ sql/src/create_tables.sql
@@ -1,20 +1,20 @@
-DROP TABLE WORK_EXPR;
-DROP TABLE EDUCATIONAL_DETAILS;
-DROP TABLE MESSAGE;
-DROP TABLE CONNECTION_USR;
-DROP TABLE USR;
+DROP TABLE WORK_EXPR CASCADE;
+DROP TABLE EDUCATIONAL_DETAILS CASCADE;
+DROP TABLE MESSAGE CASCADE;
+DROP TABLE CONNECTION_USR CASCADE;
+DROP TABLE USR CASCADE;
 
 
 CREATE TABLE USR(
-        userId varchar(20) UNIQUE NOT NULL,
-        password varchar(10) NOT NULL,
+        userId varchar(30) UNIQUE NOT NULL,
+        password varchar(30) NOT NULL,
         email text NOT NULL,
         name char(50),
         dateOfBirth date,
         Primary Key(userId));
 
 CREATE TABLE WORK_EXPR(
-        userId char(10) NOT NULL,
+        userId char(30) NOT NULL,
         company char(50) NOT NULL,
         role char(50) NOT NULL,
         location char(50),
@@ -24,7 +24,7 @@ CREATE TABLE WORK_EXPR(
         FOREIGN KEY (userId) REFERENCES USR);
 
 CREATE TABLE EDUCATIONAL_DETAILS(
-        userId char(10) NOT NULL,
+        userId char(30) NOT NULL,
         instituitionName char(50) NOT NULL,
         major char(50) NOT NULL,
         degree char(50) NOT NULL,
@@ -35,8 +35,8 @@ CREATE TABLE EDUCATIONAL_DETAILS(
 
 CREATE TABLE MESSAGE(
         msgId integer UNIQUE NOT NULL,
-        senderId char(10) NOT NULL,
-        receiverId char(10) NOT NULL,
+        senderId char(30) NOT NULL,
+        receiverId char(30) NOT NULL,
         contents char(500) NOT NULL,
         sendTime timestamp,
         deleteStatus integer,
@@ -46,18 +46,9 @@ CREATE TABLE MESSAGE(
         FOREIGN KEY (receiverId) REFERENCES USR(userId));
 
 CREATE TABLE CONNECTION_USR(
-        userId char(10) NOT NULL,
-        connectionId char(10) NOT NULL,
+        userId char(30) NOT NULL,
+        connectionId char(30) NOT NULL,
         status char(30) NOT NULL,
         PRIMARY KEY(userId,connectionId),
         FOREIGN KEY (userId) REFERENCES USR
         );
-~                                                                                                  
-~                                                                                                  
-~                                                                                                  
-~                                                                                                  
-~                                                                                                  
-~                                                                                                  
-~                                                                                                  
-~                                                                                                  
--- INSERT --                                                                     44,3-10       All
