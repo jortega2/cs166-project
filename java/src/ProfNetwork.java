7b562ca7f78695faa2f2258e111c4e6bdea2189b
jortega2 <juvenal.a.ortega@gmail.com>
2022-03-16 20:37:00 -0700

    hiding userid from friendslist

--- java/src/ProfNetwork.java
+++ java/src/ProfNetwork.java
@@ -384,8 +384,8 @@ public class ProfNetwork {
 public static void FriendList(ProfNetwork esql){
    try{
       boolean menu = true;
-      String friendlist_query = String.format("SELECT u.name, c.userId FROM CONNECTION_USR c, USR u WHERE c.connectionId = '%s' AND u.userId = c.userId"  +
-      " UNION SELECT u.name, c.connectionId from CONNECTION_USR c, USR u WHERE c.userId = '%s' AND u.userId = c.connectionId", usr, usr);
+      String friendlist_query = String.format("SELECT u.name FROM CONNECTION_USR c, USR u WHERE c.connectionId = '%s' AND u.userId = c.userId"  +
+      " UNION SELECT u.name from CONNECTION_USR c, USR u WHERE c.userId = '%s' AND u.userId = c.connectionId", usr, usr);
       String friendlist = String.format("SELECT c.userId FROM CONNECTION_USR c, USR u WHERE c.connectionId = '%s' AND u.userId = c.userId"  +
       " UNION SELECT c.connectionId from CONNECTION_USR c, USR u WHERE c.userId = '%s' AND u.userId = c.connectionId", usr, usr);
       esql.executeQueryAndPrintResult(friendlist_query);
